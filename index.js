39c514e42c8e141edcbc3566b8c388af6b8810c1
batman1292 <batman12922@gmail.com>
2016-04-18 02:49:01 +0700

    web flow

--- index.js
+++ index.js
@@ -12,6 +12,36 @@ router.get("/",function(req,res){
   res.render(path + "index.ejs");
 });
 
+router.get("/index",function(req,res){
+  res.render(path + "index.ejs");
+});
+
+router.get("/shelfs/:provice",function(req,res,next){
+  var provice = req.params.provice;
+  res.locals.stuff = {
+    provice : req.params.provice,
+  }
+  console.log(provice);
+  res.render(path + "shelfs.ejs");
+});
+
+router.get("/map/:provice/:type",function(req,res){
+  var provice = req.params.provice;
+  var type = req.params.type;
+  res.locals.stuff = {
+    provice : req.params.provice,
+    type : type,
+  }
+  res.render(path + "main_map.ejs");
+});
+// app.user(function(req, res, next) {
+  //  res.locals.stuff = {
+  //      provice : req.params.provice,
+  //  }
+//
+//    next();
+// });
+
 // router.get("/about",function(req,res){
 //   res.sendFile(path + "about.html");
 // });
@@ -22,6 +52,7 @@ router.get("/",function(req,res){
 
 app.use("/",router);
 app.use("/css",express.static(__dirname + "/css"));
+app.use("/img",express.static(__dirname + "/img"));
 // app.use("*",function(req,res){
 //   res.sendFile(path + "404.html");
 // });
